9f1721deae9c4ddc1eaa557da29e659065241c60
allisonvinicius <allisonvinicius63@gmail.com>
2022-07-13 10:26:45 -0400

    feat: include class Employee

---
+++ backend-api-rest-spring/src/main/java/net/javaguides/backend/model/Employee.java
@@ -0,0 +1,13 @@
+package package net.javaguides.backend.model;
+import lombok.Getter;
+
+@Getter
+public class Employee {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+
+}
+
